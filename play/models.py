3f79acb142af487f7e1fba3996308088bfa1fdb2
Shawn Baugh II <shawnbaughcreative@gmail.com>
2017-06-26 12:39:42 -0500

    created user validations. created migrations for models. created loin and registraion form. updated urls to serve up templates. updated views with registratation route.

--- play/models.py
+++ play/models.py
@@ -2,7 +2,39 @@ from __future__ import unicode_literals
 
 from django.db import models
 import re, bcrypt
-# Create your models here.
+# models below.
+# USER MANAGER
+class UserManager(models.Manager):
+    def register_validate(self, post):
+        result = {'errors': []}
+        if len(post['first_name']) < 2:
+            result['errors'].append('Please enter your full first name.')
+        elif len(post['last_name']) < 2:
+            result['errors'].append('Please enter your full last name.')
+        elif not re.search(r'\w+\@\w+\.\w+', post.get('email')):
+            result['errors'].append('Please enter a valid email.')
+        elif len(post['password']) < 4:
+            result['errors'].append('Please Enter a longer password.')
+        elif post['password_confirmation'] != post['password']:
+            result['errors'].append('Passwords must match')
+        return result
+    # END REGISTER VALIDATE
+    def create_user(self, post):
+        user = User.objects.create(first_name=post.get('first_name'), last_name=post.get('last_name'), email=post.get('email'), password=bcrypt.hashpw(post.get('password').encode(), bcrypt.gensalt()))
+        return user
+    # END CREATE USER
+    def login_validate(self, post):
+        user = User.objects.filter(email=post.get('email')).first()
+        if user and bcrypt.hashpw(post.get('password').encode(), user.password.encode()) == user.password:
+            return {'status': True, 'user': user}
+        else:
+            return {'status': False, 'message': 'Please enter valid credentials'}
+    # END LOGIN VALIDATE
+    def login(self, request, user):
+        if('user_id' not in request.session):
+            request.session['user_id'] = user.id
+    # END LOGIN
+# END USER MANAGER
 
 # USER MODEL
 class User(models.Model):
@@ -15,48 +47,25 @@ class User(models.Model):
 
 # SPORT MODEL
 class Sport(models.Model):
-    name = models.CharField(mac_length=255)
+    name = models.CharField(max_length=255)
+    users = models.ManyToManyField(User, related_name='sports')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 # TEAM MODEL
 class Team(models.Model):
     name = models.CharField(max_length=255)
+    users = models.ManyToManyField(User, related_name="teams")
+    sport = models.ForeignKey(Sport, related_name='teams')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 # EVENT MODEL
 class Event(models.Model):
     name = models.CharField(max_length=255)
-    sport = models.ForeignKey(Sport, related_name='users')
-    user = models.ForeignKey(User, related_name='events')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-# USER_TEAM MODEL
-class User_Team(models.Model):
-    user = models.ForeignKey(User, related_name='teams')
-    team = models.ForeignKey(Team, related_name='users')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-# USER_SPORT MODEL
-class User_Sport(models.Model):
-    user = models.ForeignKey(User, related_name='sports')
-    sport = models.ForeignKey(Sport, related_name='users')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-# SPORT_TEAM MODEL
-class Sport_Team(models.Model):
-    user = models.ForeignKey(User, related_name='teams')
-    team = models.ForeignKey(Team, related_name='users')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-# USER_EVENT TABLE
-class User_Event(models.Model):
-    user = models.ForeignKey(User, related_name='events')
-    event = models.ForeignKey(Event, related_name='users')
+    sport = models.ForeignKey(Sport, related_name='events')
+    user = models.ForeignKey(User, related_name='user_events')
+    users = models.ManyToManyField(User, related_name='events')
+    teams = models.ManyToManyField(Team, related_name='events')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- play/views.py
+++ play/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
+from django.contrib import messages
 # Create your views here.
 
 # INDEX ROUTE
 def index(request):
     return render (request, 'play/index.html')
+# END INDEX ROUTE
+# REGISTER USER
+def process(request):
+    is_valid = User.objecst.register_validate(request.POST)
+    if type(is_valid) == dict:
+        for error in is_valid['errors']:
+            messages.error(request, error)
+            return redirect('/')
+    else:
+        user = User.objects.create_user(request.POST)
+        return render(request, 'play/home.html')
